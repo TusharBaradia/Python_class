365c801f919b8b9d6c9610149d44f47e9c4de1a8
Tushar Baradia <baradia.tushar@gmail.com>
2026-01-03 23:36:10 +0530

    dictionary

---
+++ rough_dict.py
@@ -0,0 +1,15 @@
+
+
+d = {}
+d["name"] = "Tushar"
+d["age"] = 30
+d["city"] = "Indore"
+
+print(d)
+# user = ["Tushar",31,"Indore"]
+
+# print(user)
+
+
+# count helloworldthis is new world
+# dit = {"h":"value","l":"value"}
